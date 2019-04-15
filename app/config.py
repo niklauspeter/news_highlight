ea495ffe32512b0a9debde9b0127c99442567001
klaus <oriokiklaus@gmail.com>
2019-04-15 18:33:52 +0300

    adjust news api base to fetch apiKey

--- app/config.py
+++ app/config.py
@@ -2,8 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/news/{}?api_key={}
-
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/{}?country=us&apiKey={}'
 
 
 class ProdConfig(Config):
