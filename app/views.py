7f98b0effd5b8c0d48d3adef648910bc1a19de14
klaus <oriokiklaus@gmail.com>
2019-04-15 18:37:54 +0300

    add function to index view function to fetch headlines from the api

--- app/views.py
+++ app/views.py
@@ -1,21 +1,25 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-    title ="welcome to news highlights"
-    return render_template('index.html', title= title)
+
+    # Getting news headlines
+    top_headlines = get_news('top-headlines')
+    
+    title = 'Home - Welcome to The best news hightlight Website Online'
+    return render_template('index.html', title=title, headlines=top_headlines)
+
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
-
     '''
     View movie page function that returns the movie details page and its data
     '''
-    message = "you are the one "
-    return render_template('news.html',id = news_id)
+    
+    return render_template('news.html', id=news_id)
