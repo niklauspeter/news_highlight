bde40b2ba8dfbb4ac16856e41816a66e2d2c5e6d
klaus <oriokiklaus@gmail.com>
2019-04-15 18:35:03 +0300

    add process results function to create new objects for api aquired list

--- app/request.py
+++ app/request.py
@@ -1,4 +1,57 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+
+
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of movie objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        author = news_item.get('author')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        urlToImage = news_item.get('urlToImage')
+        publishedAt = news_item.get('publishedAt')
+        content=news_item.get('content')
+        if urlToImage:
+            news_object = News(id,author,description,url,urlToImage, publishedAt, content)
+            news_results.append(news_object)
+
+    return news_results
+
+     
