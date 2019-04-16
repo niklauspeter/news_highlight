d5a9de4d8f78bc118ea8e9ac5939f418f2477daf
klaus <oriokiklaus@gmail.com>
2019-04-16 09:26:13 +0300

    add request function for the sources api model

--- app/request.py
+++ app/request.py
@@ -3,12 +3,16 @@ import urllib.request,json
 from .models import news
 
 News = news.News
+Sources= news.Sources
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
-# Getting the movie base url
+# Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+#getting the news source url
+source_url = app.config['NEWS_SOURCES_BASE_URL']
 
 def get_news(category):
     '''
@@ -54,4 +58,46 @@ def process_results(news_list):
 
     return news_results
 
-     
+def get_sources(category):
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_sources_url = source_url.format(category,api_key)
+
+	with urllib.request.urlopen(get_sources_url) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+
+		sources_results = None
+
+		if get_sources_response['sources']:
+			sources_results_list = get_sources_response['sources']
+			sources_results = process_sources(sources_results_list)
+
+	return sources_results
+
+def process_sources(sources_list):
+	'''
+	Function that processes the news sources results and turns them into a list of objects
+	Args:
+		sources_list: A list of dictionaries that contain sources details
+	Returns:
+		sources_results: A list of sources objects
+	'''
+	sources_results = []
+
+	for source_item in sources_list:
+		id = source_item.get('id') 
+		name = source_item.get('name')
+		description = source_item.get('description')
+		url = source_item.get('url')
+		category = source_item.get('category')
+		language = source_item.get('language')
+		country = source_item.get('country')
+
+
+		sources_object = Sources(id,name,description,url,category,country,language)
+		sources_results.append(sources_object)
+
+
+	return sources_results
